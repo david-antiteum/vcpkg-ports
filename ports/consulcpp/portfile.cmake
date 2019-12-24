0714678e2b9b9eaac13e07c73ab98f5100c88404
David Garcia <david.garcia@antiteum.com>
2019-12-24 15:02:59 +0100

    consulcpp

---
+++ ports/consulcpp/portfile.cmake
@@ -0,0 +1,29 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO david-antiteum/consulcpp
+    REF v0.1.0-3
+    SHA512 87eaf0298e38620ec36f656e54448f0f340ff8749ae21653e053d9b28db96159d6b5cfbf6564f5763deed948d90d378593fa380a3bc6a839fb7c8ccf550e474a
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_EXAMPLES=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/consulcpp RENAME copyright)
+
+# Remove duplicate info
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# CMake integration test
+vcpkg_test_cmake(PACKAGE_NAME ${PORT})
