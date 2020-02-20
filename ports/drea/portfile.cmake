a380fa8bd9d112347854b0706b8059e9e027ae2f
David Garcia <david.garcia@antiteum.com>
2020-02-20 23:31:17 +0100

    Update for Windows support

--- ports/drea/portfile.cmake
+++ ports/drea/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO david-antiteum/drea
-    REF v0.0.1
-    SHA512 146969e9feb2e9935463cd79819ceadd70b855c0e4b84ae37d882bb8a88ca1559e62a95965c5f89ff1e88b26c3c0311808ce64d18ad3db4501afe1f29419025a
+    REF v0.0.1-1
+    SHA512 95be93e466bc46e722e8ed74ec63521eb75a98a99a799f77c407750348421fe0c34e175468b4edfaea9ca2549b00d4b52da7ad5475a3b030914708aefbce5832
     HEAD_REF master
 )
 
@@ -13,6 +13,7 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         -DBUILD_EXAMPLES=OFF
+        -DBUILD_TOOLS=OFF
 )
 
 vcpkg_install_cmake()
@@ -20,10 +21,6 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/drea RENAME copyright)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 # Remove duplicate info
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
