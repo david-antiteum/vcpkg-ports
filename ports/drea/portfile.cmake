cec239cc5495b634e1d9e61022abe31c7516f594
David Garcia <david.garcia@antiteum.com>
2020-03-25 12:55:36 +0100

    Version 0.0.3-1

--- ports/drea/portfile.cmake
+++ ports/drea/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO david-antiteum/drea
-    REF v0.0.2-1
-    SHA512 a3250722ed2e049988d756bd7d0de4e77eca1a9197ace05f9ab975811fd2e25f2c5dc78b0e5adb2661d40379ef25b532e7182cf68ca92d32225276c1457830eb
+    REF v0.0.3-1
+    SHA512 39c4bd98344f156bcdc545bdf0a836a86fe3aeaa92319dc5403f5a8384790b05d0a7cd9786631c3fe4c3f8990ae89c646acf41cad2e0d7a75c9a7cf6d8c330dd
     HEAD_REF master
 )
 
